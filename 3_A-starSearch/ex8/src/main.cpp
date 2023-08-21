9bc5746df2af84420f164b3644f70204250191fc
Eslam Sherif <eslam.sherif5@gmail.com>
2023-08-22 00:05:47 +0400

    lesson3_ex8

--- 3_A-starSearch/ex8/src/main.cpp
+++ 3_A-starSearch/ex8/src/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+using std::abs;
 using std::cout;
 using std::ifstream;
 using std::istringstream;
@@ -15,54 +16,35 @@ enum class State
     kObstacle
 };
 
-// TODO: Change the return type to use auto or
-// explicitly indicate vector<State>
-// DONE
 vector<State> ParseLine(string line)
 {
     istringstream sline(line);
     int n;
     char c;
-    // TODO: Change the variable type for row
-    // to be a vector<State>
-    // DONE
     vector<State> row;
     while (sline >> n >> c && c == ',')
     {
-        // TODO: Modify the line below to push_back
-        // a State::kEmpty if n is 0, and push_back
-        // a State::kObstacle otherwise.
-        // DONE
-        switch (n)
+        if (n == 0)
         {
-        case 0:
             row.push_back(State::kEmpty);
-            break;
-        default:
+        }
+        else
+        {
             row.push_back(State::kObstacle);
-            break;
         }
     }
     return row;
 }
 
-// TODO: Modify the return type here as well. Just
-// as above, the board will contain State objects
-// instead of ints.
-// DONE
 vector<vector<State>> ReadBoardFile(string path)
 {
     ifstream myfile(path);
-    // TODO: Modify the board declarationto store
-    // State objects instead of ints.
     vector<vector<State>> board{};
     if (myfile)
     {
         string line;
         while (getline(myfile, line))
         {
-            // TODO: Modify the row type to use State
-            // objects instead of ints.
             vector<State> row = ParseLine(line);
             board.push_back(row);
         }
@@ -70,8 +52,14 @@ vector<vector<State>> ReadBoardFile(string path)
     return board;
 }
 
-// TODO: Modify the function signature below to accept
-// cells with State type instead of int type.
+// TODO: Write the Search function stub here.
+std::vector<vector<State>> Search(std::vector<vector<State>> copy_Board, int start[2], int goal[2])
+{
+    std::cout << "No path found!" << std::endl;
+    std::vector<vector<State>> board_with_path;
+    return board_with_path;
+}
+
 string CellString(State cell)
 {
     switch (cell)
@@ -89,8 +77,6 @@ void PrintBoard(const vector<vector<State>> board)
     {
         for (int j = 0; j < board[i].size(); j++)
         {
-            // TODO: Modify the line below to call CellString
-            // on each element of the board before streaming to cout.
             cout << CellString(board[i][j]);
         }
         cout << "\n";
@@ -99,6 +85,15 @@ void PrintBoard(const vector<vector<State>> board)
 
 int main()
 {
+    // TODO: Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
+    int init[2] = {0,0};
+    int goal[2] = {4,5};
+
     auto board = ReadBoardFile("1.board");
-    PrintBoard(board);
+
+    // TODO: Call Search with "board", "init", and "goal". Store the results in the variable "solution".
+    auto solution = Search(board, init, goal);
+
+    // TODO: Change the following line to pass "solution" to PrintBoard.
+    PrintBoard(solution);
 }
